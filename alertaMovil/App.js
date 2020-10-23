440dcf8f11900aa4643c94786c32600594ce930b
Sebastián Villanueva <svillazamora@outlook.com>
2020-10-22 22:44:34 -0600

    Arreglos a alertas

--- alertaMovil/App.js
+++ alertaMovil/App.js
@@ -7,15 +7,16 @@ import HomeScreen from './screens/HomeScreen';
 import ExampleScreen from './screens/ExampleScreen'; // 2 paso
 import AlertsScreen from './screens/AlertsScreen';
 
+
 const RootStack = createStackNavigator();
  
 const App = () => {
   return (
     <NavigationContainer>
       <RootStack.Navigator>
+        <RootStack.Screen name="Example" component={ExampleScreen} />
         <RootStack.Screen name="Landing" component={LandingScreen} />
         <RootStack.Screen name="Home" component={HomeScreen} />
-        <RootStack.Screen name="Example" component={ExampleScreen} /> 
         <RootStack.Screen name="Alerts" component={AlertsScreen} /> 
       </RootStack.Navigator>
     </NavigationContainer>
