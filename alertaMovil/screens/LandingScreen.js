197cb725adb893013beb1e4cfb51c7705cde98c2
María José Cortés <maria2000jose.mj@gmail.com>
2020-10-24 15:55:32 -0600

    last changes

--- alertaMovil/screens/LandingScreen.js
+++ alertaMovil/screens/LandingScreen.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
- 
+import React from "react";
+import { View, Text, Button, StyleSheet, Image } from "react-native";
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#C2D1D9'
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#C2D1D9",
   },
 });
- 
-const LandingScreen = ({navigation}) => {
+
+const LandingScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Public Landing Screen</Text>
+      <Image
+        style={{ width: 225, height: 219 }}
+        source={require("../assets/CNE-logo2.png")}
+      />
       <Button
-        title="Go to Home"
-        title="Enter"
-        onPress={() => navigation.navigate('LogIn')}
+        title="Ingresar a la aplicación"
+        title="Ingresar"
+        onPress={() => navigation.navigate("LogIn")}
       />
     </View>
   );
 };
- 
+
 export default LandingScreen;
