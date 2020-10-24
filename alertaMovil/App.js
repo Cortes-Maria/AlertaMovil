8ab9c1546099ef73669173f70e20ebdc5e99561e
Sebastián Villanueva <svillazamora@outlook.com>
2020-10-23 22:39:44 -0600

    Menu del lado terminado
    Hice el menu de el lado con styling

--- alertaMovil/App.js
+++ alertaMovil/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import { Button, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
- 
-import LandingScreen from './screens/LandingScreen';
-import HomeScreen from './screens/HomeScreen';
-import ExampleScreen from './screens/ExampleScreen'; // 2 paso
+import React from "react";
+import { Button, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-import AlertsScreen from './screens/AlertsScreen';
+import LandingScreen from "./screens/LandingScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ExampleScreen from "./screens/ExampleScreen"; // 2 paso
 
-import LogIn from './screens/LogIn';
-import SignUp from './screens/SignUp';
+import AlertsScreen from "./screens/AlertsScreen";
 
+import LogIn from "./screens/LogIn";
+import SignUp from "./screens/SignUp";
 
+import { createDrawerNavigator, DrawerContent } from "@react-navigation/drawer";
 
-const RootStack = createStackNavigator();
- 
+const Drawer = createDrawerNavigator();
+
+const LandingStack = createStackNavigator();
+const HomeStack = createStackNavigator();
+const AlertsStack = createStackNavigator();
+const LogInStack = createStackNavigator();
+const SignUpStack = createStackNavigator();
+const ExampleStack = createStackNavigator();
+
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = React.useState(false);
 
- 
   const handleLogIn = () => {
     // TODO implement real sign in mechanism
 
@@ -28,7 +35,7 @@ const App = () => {
 
   const handleSignOut = () => {
     // TODO implement real sign out mechanism
- 
+
     setIsAuthenticated(false);
   };
 
@@ -38,46 +45,120 @@ const App = () => {
     setIsAuthenticated(true);
   };
 
-
-  return (
-    <NavigationContainer>
-      <RootStack.Navigator> 
+  const HomeStackScreen = ({ navigation }) => {
+    return (
+      <HomeStack.Navigator>
         {isAuthenticated ? (
-          <RootStack.Screen 
-            name="Home" 
-            component={HomeScreen} 
+          <HomeStack.Screen
+            name="Alertas"
+            component={AlertsScreen}
             options={{
+              headerStyle: {
+                backgroundColor: "#31F0AB",
+              },
+              headerTintColor: "white",
+              headerLeft: () => (
+                <Icon
+                  style={{ marginLeft: 10 }}
+                  name="navicon"
+                  size={30}
+                  color="white"
+                  onPress={() => navigation.openDrawer()}
+                />
+              ),
               headerRight: () => (
-                <Button onPress={handleSignOut} title="Log Out" />
+                <Icon
+                  style={{ marginRight: 10 }}
+                  name="home"
+                  size={30}
+                  color="white"
+                />
               ),
             }}
           />
-          ) : (
-            <>
-              <RootStack.Screen 
-                name="Landing" 
-                component={LandingScreen} 
-                options={{
-                  animationTypeForReplace: 'pop',
-                }}
+        ) : (
+          <>
+            <HomeStack.Screen
+              name="Landing"
+              component={LandingScreen}
+              options={{
+                animationTypeForReplace: "pop",
+              }}
+            />
+
+            <HomeStack.Screen
+              name="LogIn"
+              options={{
+                headerStyle: {
+                  backgroundColor: "#31F0AB",
+                },
+                headerTintColor: "white",
+                headerLeft: () => <></>,
+              }}
+            >
+              {(props) => <LogIn {...props} onLogIn={handleLogIn} />}
+            </HomeStack.Screen>
+            <HomeStack.Screen
+              name="SignUp"
+              options={{
+                headerStyle: {
+                  backgroundColor: "#31F0AB",
+                },
+                headerTintColor: "white",
+              }}
+            >
+              {(props) => <SignUp {...props} onSignUp={handleSignUp} />}
+            </HomeStack.Screen>
+          </>
+        )}
+        <HomeStack.Screen
+          name="Example"
+          component={ExampleScreen}
+          options={{
+            headerStyle: {
+              backgroundColor: "#31F0AB",
+            },
+            headerTintColor: "white",
+            headerLeft: () => (
+              <Icon
+                style={{ marginLeft: 10 }}
+                name="navicon"
+                size={30}
+                color="white"
+                onPress={handleSignOut}
               />
-              <RootStack.Screen name="LogIn">
-                {(props) => (
-                  <LogIn {...props} onLogIn={handleLogIn}/>
-                )}
-              </RootStack.Screen>
-              <RootStack.Screen name="SignUp">
-                {(props) => (
-                  <SignUp {...props} onSignUp={handleSignUp}/>
-                )}
-              </RootStack.Screen>
-            </>
-          )}
-        <RootStack.Screen name="Example" component={ExampleScreen} />
-        <RootStack.Screen name="Alerts" component={AlertsScreen} />
-      </RootStack.Navigator>
+            ),
+            headerRight: () => (
+              <Icon
+                style={{ marginRight: 10 }}
+                name="home"
+                size={30}
+                color="white"
+              />
+            ),
+          }}
+        />
+
+        <HomeStack.Screen name="Home" component={HomeScreen} />
+      </HomeStack.Navigator>
+    );
+  };
+
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        drawerStyle={{ backgroundColor: "#C2D1D9", width: 240 }}
+        drawerContentOptions={{
+          labelStyle: { color: "black" },
+          activeBackgroundColor: "#10d88f",
+          inactiveBackgroundColor: "#10d88f",
+        }}
+      >
+        <Drawer.Screen name="Zonas de interés" component={HomeStackScreen} />
+        <Drawer.Screen name="Cerrar sesión" component={HomeStackScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 };
- 
+
 export default App;
--- alertaMovil/screens/AlertsScreen.js
+++ alertaMovil/screens/AlertsScreen.js
@@ -1,73 +1,63 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet , TouchableOpacity} from 'react-native';
-import { Card, Header} from 'react-native-elements'
+import React from "react";
+import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
+import { Card, Header } from "react-native-elements";
 
 //estos son los estilos como el css por decirlo asi
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#C2D1D9'
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#C2D1D9",
   },
 });
 
 const alerts = [
-    {
-        place: 'Parrita', 
-        disaster: 'Inundacion',
-        date: '20/10/20',
-        time: '10:00 pm'           
-    },
-    {
-        place: 'Alajuela', 
-        disaster: 'Temblor',
-        time: '10:00 pm'           
-    },
-    {
-        place: 'Chepe', 
-        disaster: 'Chepe',
-        time: '10:00 pm'           
-    }
-]
+  {
+    place: "Parrita",
+    disaster: "Inundacion",
+    date: "20/10/20",
+    time: "10:00 pm",
+  },
+  {
+    place: "Alajuela",
+    disaster: "Temblor",
+    time: "10:00 pm",
+  },
+  {
+    place: "Chepe",
+    disaster: "Chepe",
+    time: "10:00 pm",
+  },
+];
 
-const AlertsScreen = ({navigation}) => {
+const AlertsScreen = ({ navigation }) => {
   return (
-    
     <View style={styles.container}>
-        <Header
-            placement="left"
-            leftComponent={{ icon: 'menu', color: '#fff' }}
-            centerComponent={{ text: 'Alerta Movil', style: { color: '#fff' } }}
-            rightComponent={{ icon: 'home', color: '#fff' }}
-            containerStyle={{
-                backgroundColor: '#31F0AB',
-                justifyContent: 'space-around',
-            }}
-        ex/>
-        <Text style={styles.Title} style={{fontSize: 20, textAlign: 'left', alignSelf: 'stretch'}}>Alertas recientes</Text>
-        {
-            alerts.map((u,i)=>{
-                return(
-                <TouchableOpacity key={i} onPress={() => navigation.navigate('Home')}>
-                    <Card>
-                        <Card.Title>{u.disaster}</Card.Title>
-                        <Card.Divider/>
-                        { 
-                            <View key={i} style={styles.user}>
-                                <Text style={styles.name}>{u.place}</Text>
-                                <Text style={styles.name}>{u.time}</Text>
-                            </View>  
-                        }
-                    </Card>
-                </TouchableOpacity>
-                );
-                
-            })
-        }
-        
+      <Text
+        style={styles.Title}
+        style={{ fontSize: 20, textAlign: "left", alignSelf: "stretch" }}
+      >
+        Alertas recientes
+      </Text>
+      {alerts.map((u, i) => {
+        return (
+          <TouchableOpacity key={i} onPress={() => navigation.navigate("Home")}>
+            <Card>
+              <Card.Title>{u.disaster}</Card.Title>
+              <Card.Divider />
+              {
+                <View key={i} style={styles.user}>
+                  <Text style={styles.name}>{u.place}</Text>
+                  <Text style={styles.name}>{u.time}</Text>
+                </View>
+              }
+            </Card>
+          </TouchableOpacity>
+        );
+      })}
     </View>
   );
 };
- 
+
 export default AlertsScreen;
